81e42a89ddbf985aadaf84bca3830165fb0abd01
Vlad <vladvoytkiv321@gmail.com>
2024-10-22 20:49:29 +0300

    inittial

---
+++ movies/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=100)
+    director = models.CharField(max_length=100)
+    actors = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='movies/media')
+
+
+class Session(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    date_of_session = models.DateField()
+    quantity = models.IntegerField()
+    ticket_price = models.DecimalField(max_digits=5, decimal_places=2)
